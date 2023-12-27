edc056b68a23cd1debc0136cfe5a6ddc57358799
beanbeanchris <christophercastroramirez@gmail.com>
2023-12-27 10:37:51 -0800

    create empty files for fragments.

---
+++ app/src/main/java/edu/gatech/seclass/ecommerence_app/fragments/RegisterFragment.kt
@@ -0,0 +1,7 @@
+package edu.gatech.seclass.ecommerence_app.fragments
+
+import androidx.fragment.app.Fragment
+import edu.gatech.seclass.ecommerence_app.R
+
+class RegisterFragment: Fragment(R.layout.fragment_register) {
+}
