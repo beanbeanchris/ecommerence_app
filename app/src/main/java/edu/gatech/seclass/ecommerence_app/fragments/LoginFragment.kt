a050876d3827c8be3e87e9c0eb34a0e85b70cca6
beanbeanchris <christophercastroramirez@gmail.com>
2023-12-28 17:31:59 -0800

    create login and account options UI.

--- app/src/main/java/edu/gatech/seclass/ecommerence_app/fragments/LoginFragment.kt
+++ app/src/main/java/edu/gatech/seclass/ecommerence_app/fragments/LoginFragment.kt
@@ -3,5 +3,5 @@ package edu.gatech.seclass.ecommerence_app.fragments
 import androidx.fragment.app.Fragment
 import edu.gatech.seclass.ecommerence_app.R
 
-class LoginFragment: Fragment(R.layout.fragment_login) {
+class LoginFragment: Fragment(R.layout.fragment_introduction) {
 }
