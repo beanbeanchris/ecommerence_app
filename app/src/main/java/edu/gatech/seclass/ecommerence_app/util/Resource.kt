e07fd9eda3178986aa7a707f63ffdb7952d7377d
beanbeanchris <christophercastroramirez@gmail.com>
2024-01-03 16:53:16 -0800

    add comments.

--- app/src/main/java/edu/gatech/seclass/ecommerence_app/util/Resource.kt
+++ app/src/main/java/edu/gatech/seclass/ecommerence_app/util/Resource.kt
@@ -1,5 +1,10 @@
 package edu.gatech.seclass.ecommerence_app.util
 
+// A sealed class named 'Resource' with a generic type 'T'. This class is used to represent
+// the different states of data handling (like loading, success, or error) in a standardized way,
+// particularly useful for network operations or database transactions.
+// For UI state handling in MVVM architecture
+
 sealed class Resource<T> (
     val data: T? = null,
     val message: String? = null
