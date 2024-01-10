09c37ee9057a91b166eefad5207f173b5bb98bc3
beanbeanchris <christophercastroramirez@gmail.com>
2024-01-09 16:58:31 -0800

    Update graph and login/register.

--- app/src/main/java/edu/gatech/seclass/ecommerence_app/activities/LoginRegisterActivity.kt
+++ app/src/main/java/edu/gatech/seclass/ecommerence_app/activities/LoginRegisterActivity.kt
@@ -1,4 +1,13 @@
 package edu.gatech.seclass.ecommerence_app.activities
 
-class LoginRegisterActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import edu.gatech.seclass.ecommerence_app.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+
 }
